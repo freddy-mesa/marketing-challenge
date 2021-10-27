2d90d1f0f627fd8b8d62893164db3f4965cb34a2
Freddy Mesa <mesa.freddy@gmail.com>
2021-10-27 00:09:42 -0400

    Delete .aws-sam directory

--- .aws-sam/build/ReceeveChallengeFunction/challenge/src/dao/database.ts
+++
@@ -1,6 +0,0 @@
-import { EventEntity } from "./event.entity";
-
-export interface Database {
-    put(event: EventEntity) : Promise<EventEntity|undefined> 
-    get(id: string) : Promise<EventEntity|undefined> 
-}
